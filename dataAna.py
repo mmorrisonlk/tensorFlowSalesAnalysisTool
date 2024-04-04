fa1552e42885e01b7fc35d497717a9ce0412ce75
Mark Morrison <mmorrisonlk@gmail.com>
2024-04-03 21:34:47 -0500

    Going to have to go another direction

--- dataAna.py
+++ dataAna.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import numpy as np
 
+import tensorflow as tf
+from tensorflow.keras import layers
+
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
 
-df = pd.read_csv(r"data\financials.csv")
+financials = pd.read_csv(r"data\financials.csv")
 
-print(df)
+print(financials.head())
 
-# import tensorflow as tf
-# from tensorflow.keras import layers
+salesAnalysis = financials.copy()
+salesDataLabels = financials.pop('Country')
 
+salesArrayAnalysis = np.array(salesAnalysis)
+
+print(salesArrayAnalysis)
+
+# salesModel = tf.keras.Sequential([
+#   layers.Dense(64, activation='relu'),
+#   layers.Dense(1)
+# ])
+
+# salesModel.compile(loss = tf.keras.losses.MeanSquaredError(),
+#                       optimizer = tf.keras.optimizers.Adam())
+
+# salesModel.fit(salesArrayAnalysis, salesDataLabels, epochs=10)
