36e0dde83cd6f0faca97c9faf9ff90c80c37341e
Mark Morrison <mmorrisonlk@gmail.com>
2024-04-06 21:04:22 -0500

    Found another approach now just to merge them together

--- model.py
+++ model.py
@@ -7,13 +7,14 @@ from tensorflow.keras import layers
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
 
+# Import the data file locally
 financials = pd.read_csv(r"data\financialsSimple.csv")
 
-print(financials.head())
-
+#Separate relevant columns into Features and Labels
 financialsFeatures = np.array(financials[['Sale Price']])
 financialsLabels = np.array(financials[['Units Sold']])
 
+#Basic Tensorflow model definition
 financialsModel = tf.keras.Sequential([
   layers.Dense(64, activation='relu'),
   layers.Dense(1)
